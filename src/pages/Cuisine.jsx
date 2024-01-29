a6e627572cc6011a2da10709e825f3ca5a441c14
noahw3 <weinstein.n@northeastern.edu>
2024-01-29 11:07:18 -0500

    rest of code

---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
+
+function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=73b5b216228143c89e42c190672a13ba&number=9&tags=vegetarian&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.type);
+    console.log(params.type);
+  }, [params.type]);
+  return (
+    <Grid
+      antimate={{ opacity: 1 }}
+      initial={{ opaciaty: 0 }}
+      exit={{ opacity: 0 }}
+    >
+      {cuisine.map((item) => {
+        return (
+          <Card key={item.id}>
+            <Link to={"/recipe/" + item.id}>
+              <img src={item.image} alt="" />
+              <h4>{item.title}</h4>
+            </Link>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
+}
+
+const Grid = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-grap: 3rem;
+`;
+
+const Card = styled(motion.div)`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
+
+export default Cuisine;
